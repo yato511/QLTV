f2224700463ee2e1ceb07445695934661aced4de
Tran Quoc Toan <tranquoctoan51199@gmail.com>
2020-08-17 03:57:05 +0700

    Add Admin Views

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/index.js");
 const multer = require("multer");
+const book = require("../models/book.js");
 
 router.get("/", async (req, res) => {
 	res.redirect("/admin/dashboard");
@@ -13,79 +14,106 @@ router.get("/dashboard", async (req, res) => {
 	});
 });
 router.get("/users", async (req, res) => {
-	const users = await db.user.findAll({
+	let users = await db.user.findAll({
 		include: [
 			{
-				model: db.black_list
-			}
-		]
-	})
-	res.json(users)
+				model: db.black_list,
+			},
+		],
+		raw: true,
+		nest: true,
+	});
+	users = users.map((user, index) => ({ ...user, index: index + 1 }));
+	res.render("admin/users", {
+		title: "Quản lý độc giả",
+		layout: "adminLayout.hbs",
+		list: users,
+	});
 });
 
 router.get("/users/:id", async (req, res) => {
 	const { id } = req.params;
+	const user = await db.user.findOne({
+		where: {
+			id: id,
+		},
+		raw: true,
+	});
 	const borrows = await db.borrow_detail.findAll({
 		where: {
-			userId: id
+			userId: id,
 		},
 		include: [
 			{
 				model: db.book,
-				include: [
-					{
-						model: db.category
-					}
-				]
 			},
-			{
-				model: db.user
-			}
-		]
-	})
-	res.json(borrows);
-})
+		],
+		nest: true,
+		raw: true,
+	});
+
+	const list = borrows.map((item, index) => ({
+		index: index + 1,
+		...item,
+		isOver: true,
+	}));
+	const data = { ...user, list };
+	console.log(list);
+	res.render("admin/usersDetail", {
+		title: "Quản lý độc giả",
+		layout: "adminLayout.hbs",
+		data,
+	});
+});
 router.get("/books", async (req, res) => {
 	const books = await db.book.findAll({
 		include: [
 			{
-				model: db.category
-			}
-		]
+				model: db.category,
+			},
+		],
+		nest: true,
+		raw: true,
 	});
-
-	let result = [];
-
-	for (let book of books) {
-		if (book.isAvailable == 0) {
-			const borrow = await db.borrow_detail.findOne({
-				where: {
-					bookId: book.id
-				}
-			})
-
-			if (borrow) {
-				const user = await db.user.findOne({
+	let result;
+	result = await Promise.all(
+		books.map(async (book, index) => {
+			if (book.isAvailable == 0) {
+				const borrow = await db.borrow_detail.findOne({
 					where: {
-						id: borrow.userId
-					}
+						bookId: book.id,
+					},
+					raw: true,
 				});
-				book.dataValues.user = user;
+				if (borrow) {
+					const user = await db.user.findOne({
+						where: {
+							id: borrow.userId,
+						},
+						raw: true,
+					});
+					book.user = user;
+				}
 			}
-		}
-		result.push(book.dataValues);
-	}
-	return res.json(result);
+			book.index = index + 1;
+			return book;
+		})
+	);
+	res.render("admin/books", {
+		title: "Quản lý sách",
+		layout: "adminLayout.hbs",
+		list: result,
+	});
 });
 
 router.post("/books", async (req, res) => {
 	const book = await db.book.create(req.body.book);
 	const storage = multer.diskStorage({
 		filename: function (req, file, cb) {
-		  cb(null, `${book.id}.png`)
+			cb(null, `${book.id}.png`);
 		},
 		destination: function (req, file, cb) {
-		  cb(null, `./public/imgs/books/`);
+			cb(null, `./public/imgs/books/`);
 		},
 	});
 	const upload = multer({ storage });
@@ -93,9 +121,9 @@ router.post("/books", async (req, res) => {
 		if (err) {
 			res.json("error");
 		} else {
-		  res.status(200).json("success");
+			res.status(200).json("success");
 		}
-	})
+	});
 });
 
 router.put("/books/:id", async (req, res) => {
